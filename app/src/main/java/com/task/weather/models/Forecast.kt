d874778f5ef35fa0db3b6c467ab4230ddb147436
Urtė <urte.rimsaite@gmail.com>
2019-03-15 13:16:46 +0200

    Update colors

--- app/src/main/java/com/task/weather/models/Forecast.kt
+++ app/src/main/java/com/task/weather/models/Forecast.kt
@@ -6,5 +6,5 @@ import java.time.format.DateTimeFormatter
 data class Forecast (val dateTimeString: String, val conditions: String, val temp: String, val icon: String) {
     private val dateTime = LocalDateTime.parse(dateTimeString, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
     val time: String = dateTime.format(DateTimeFormatter.ofPattern("HH:mm"))
-    val dayOfWeek = dateTime.format(DateTimeFormatter.ofPattern("EEEE"))
+    val dayOfWeek: String = dateTime.format(DateTimeFormatter.ofPattern("EEEE"))
 }
